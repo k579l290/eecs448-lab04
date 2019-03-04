ab3f11a39c75f011501284c5655c90f511b5fba2
Gwen Liu <k579l290@ku.edu>
2019-03-04 14:15:30 -0600

    fixed table parse error and styling

--- Lab4/exercise3/customerBackend.php
+++ Lab4/exercise3/customerBackend.php
@@ -3,13 +3,13 @@
     error_reporting(E_ALL);
     ini_set("display_errors", 1);
 
-    $desk = $_POST["desk"];
-    $chair = $_POST["chair"];
-    $lamp = $_POST["lamp"];
-    $shipping = $_POST["shipping"];
+    $desk = $_POST['desk'];
+    $chair = $_POST['chair'];
+    $lamp = $_POST['lamp'];
+    $shipping = $_POST['shipping'];
 
-    $username = $_POST["username"];
-    $password = $_POST["password"];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
 
     if ($shipping == "7-day") {
@@ -24,18 +24,19 @@
 
     $total = ($desk * 600) + ($chair * 25) + ($lamp * 50) + $shipping_cost;
 
-    echo "<html><head><link rel="stylesheet" type="text/css" href="style.css"/></head></html>";
+    echo "<html><head><link rel='stylesheet' type='text/css' href='style.css'/></head><body>";
     echo "Welcome <br>";
     echo "Your username is: " . $username . "<br>";
     echo "Your password is: " . $password . "<br><br>";
   echo "Receipt <br>";
-  echo "<table id="r">";
-    echo "<tr> <th>Quantity</th> <th>Cost Per Item</th> <th>Sub Total</th> </tr>";
-    echo "<tr> <td>Desk</td> <td>" . $desk . "</td> <td>$600</td> <td>$" . ($desk * 600). "</td> </tr>";
-    echo "<tr> <td>Chair</td> <td>" . $chair . "</td> <td>$25</td> <td>$" . ($chair * 25) . "</td> </tr>";
-    echo "<tr> <td>Lamp</td> <td>" . $lamp . "</td> <td>$50</td> <td>$" . ($lamp * 50) . "</td> </tr>";
-    echo "<tr> <td>Shipping</td> <td colspan = "2">" . $shipping . "</td> <td>$" . $shipping_cost . "</td> </tr>";
-    echo "<tr> <td colspan = "3">Total Cost</td> <td>$" . $total . "</td> </tr>";
+  echo "<table id='r'>";
+    echo "<tr> <th></th><th>Quantity</th> <th>Cost Per Item</th> <th>Sub Total</th> </tr>";
+    echo "<tr> <th>Desk</th> <td>" . $desk . "</td> <td>$600</td> <td>$" . ($desk * 600). "</td> </tr>";
+    echo "<tr> <th>Chair</th> <td>" . $chair . "</td> <td>$25</td> <td>$" . ($chair * 25) . "</td> </tr>";
+    echo "<tr> <th>Lamp</th> <td>" . $lamp . "</td> <td>$50</td> <td>$" . ($lamp * 50) . "</td> </tr>";
+    echo "<tr> <th>Shipping</th> <td colspan = '2'>" . $shipping . "</td> <td>$" . $shipping_cost . "</td> </tr>";
+    echo "<tr> <th colspan = '3'>Total Cost</th> <th>$" . $total . "</th> </tr>";
 echo "</table>";
+echo "</body></html>";
 
 ?>
