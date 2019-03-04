2ef26baa3243269ed1d3cc362cce7891e51e5cf0
Gwen Liu <k579l290@ku.edu>
2019-03-04 14:04:34 -0600

    fixed table format

--- Lab4/exercise3/customerBackend.php
+++ Lab4/exercise3/customerBackend.php
@@ -34,7 +34,7 @@
     echo "<tr> <td>Desk</td> <td>" . $desk . "</td> <td>$600</td> <td>$" . ($desk * 600). "</td> </tr>";
     echo "<tr> <td>Chair</td> <td>" . $chair . "</td> <td>$25</td> <td>$" . ($chair * 25) . "</td> </tr>";
     echo "<tr> <td>Lamp</td> <td>" . $lamp . "</td> <td>$50</td> <td>$" . ($lamp * 50) . "</td> </tr>";
-    echo "<tr> <td>Shipping</td> <td colspan = "2">" . $shipping . "</td> <td></td> <td>$" . $shipping_cost . "</td> </tr>";
+    echo "<tr> <td>Shipping</td> <td colspan = "2">" . $shipping . "</td> <td>$" . $shipping_cost . "</td> </tr>";
     echo "<tr> <td colspan = "3">Total Cost</td> <td>$" . $total . "</td> </tr>";
 echo "</table>";
 
